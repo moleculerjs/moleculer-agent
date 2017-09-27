f7e0d636167a46481a2015ccc8008569c2aaed2f
Icebob <mereg.norbert@gmail.com>
2017-09-27 13:10:21 +0200

    Initial files

---
+++ README.md
@@ -0,0 +1,31 @@
+![Moleculer logo](docs/assets/logo.png)
+
+[![Build Status](https://travis-ci.org/ice-services/moleculer-agent.svg?branch=master)](https://travis-ci.org/ice-services/moleculer-agent)
+[![Coverage Status](https://coveralls.io/repos/github/ice-services/moleculer-agent/badge.svg?branch=master)](https://coveralls.io/github/ice-services/moleculer-agent?branch=master)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/b108c12cbf554fca9c66dd1925d11cd0)](https://www.codacy.com/app/mereg-norbert/moleculer?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ice-services/moleculer-agent&amp;utm_campaign=Badge_Grade)
+[![Code Climate](https://codeclimate.com/github/ice-services/moleculer-agent/badges/gpa.svg)](https://codeclimate.com/github/ice-services/moleculer-agent)
+[![David](https://img.shields.io/david/ice-services/moleculer-agent.svg)](https://david-dm.org/ice-services/moleculer-agent)
+[![Known Vulnerabilities](https://snyk.io/test/github/ice-services/moleculer-agent/badge.svg)](https://snyk.io/test/github/ice-services/moleculer-agent)
+[![Join the chat at https://gitter.im/ice-services/moleculer-agent](https://badges.gitter.im/ice-services/moleculer-agent.svg)](https://gitter.im/ice-services/moleculer-agent?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+[![Downloads](https://img.shields.io/npm/dt/moleculer-agent.svg)](https://www.npmjs.com/package/moleculer-agent)
+[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bhttps%3A%2F%2Fgithub.com%2Fice-services%2Fmoleculer-agent.svg?type=shield)](https://app.fossa.io/projects/git%2Bhttps%3A%2F%2Fgithub.com%2Fice-services%2Fmoleculer-agent?ref=badge_shield)
+
+# [PoC] Moleculer Agent [![NPM version](https://img.shields.io/npm/v/moleculer-agent.svg)](https://www.npmjs.com/package/moleculer-agent)
+**Proof-of-Concept** project. Don't use it in production!
+
+## Concept
+
+## Usage
+
+## Actions
+
+# License
+Moleculer is available under the [MIT license](https://tldrlegal.com/license/mit-license).
+
+[3rd party licenses](https://app.fossa.io/reports/833f0d1b-462b-4eff-a4e4-e030115439fe)
+
+# Contact
+Copyright (c) 2016-2017 Ice Services
+
+[![@ice-services](https://img.shields.io/badge/github-ice--services-green.svg)](https://github.com/ice-services) [![@MoleculerJS](https://img.shields.io/badge/twitter-MoleculerJS-blue.svg)](https://twitter.com/MoleculerJS)
---
+++ src/agent.js
@@ -0,0 +1,223 @@
+"use strict";
+
+const glob 		= require("glob");
+const path 		= require("path");
+const _ 		= require("lodash");
+const Exec 		= require("child_process").exec;
+const Spawn		= require("child_process").spawn;
+
+const { MoleculerClientError } 	= require("moleculer").Errors;
+
+module.exports = {
+	// Service name
+	name: "$agent",
+
+	// Default settings
+	settings: {
+		serviceFolder: "./examples"
+	},
+
+	actions: {
+		/**
+		 * Start a local service
+		 */
+		start: {
+			params: {
+				service: "string"
+			},
+			handler(ctx) {
+				return this.startService(ctx.params.service);
+			}
+		},
+
+		/**
+		 * Stop a local service
+		 */
+		stop: {
+			params: {
+				service: "string"
+			},
+			handler(ctx) {
+				return this.stopService(ctx.params.service);
+			}
+		},
+
+		/**
+		 * Get list of available services
+		 *
+		 * @param {any} ctx
+		 * @returns
+		 */
+		services(ctx) {
+			return Object.values(this.services).map(schema => _.pick(schema, ["name", "version", "settings", "metadata"]));
+		},
+
+		/**
+		 * Start all local services
+		 *
+		 * @param {any} ctx
+		 */
+		startAll(ctx) {
+			return this.startAllServices();
+		},
+
+		/**
+		 * Stop all local services
+		 *
+		 * @param {any} ctx
+		 */
+		stopAll(ctx) {
+			return this.stopAllServices();
+		},
+
+		/**
+		 * Start a new Moleculer NodeJS process with the same arguments
+		 *
+		 * @param {any} ctx
+		 */
+		fork(ctx) {
+			const args = [...process.execArgv, ...process.argv.slice(1)];
+			const proc = this.spawn(process.execPath, args, { detached: true, stdio: "ignore" });
+			proc.unref();
+			this.logger.info(`New process started. PID: ${proc.pid}`);
+			return { pid: proc.pid };
+		},
+
+		exec: {
+			params: {
+				cmd: "string",
+				opts: { type: "object", optional: true }
+			},
+			handler(ctx) {
+				return this.execute(ctx.params.cmd, ctx.params.opts || {});
+			}
+		},
+
+		/**
+		 * Exit the current process
+		 *
+		 * @param {any} ctx
+		 */
+		exitProcess: {
+			params: {
+				code: { type: "number", optional: true }
+			},
+			handler(ctx) {
+				this.logger.warn("Exit process...");
+				process.exit(ctx.params.code || 0);
+			}
+		}
+	},
+
+	methods: {
+		/**
+		 * Read all services from service folder
+		 *
+		 */
+		readServiceFolder() {
+			const folder = path.resolve(this.settings.serviceFolder);
+			this.logger.info(`Read all services from '${folder}' folder...`);
+			const serviceFiles = glob.sync(path.join(folder, "**", "*.service.js"));
+			this.services = {};
+			serviceFiles.forEach(file => {
+				const schema = require(file);
+				if (schema.name)
+					this.services[file] = schema;
+			});
+
+			this.logger.info(`Found ${Object.keys(this.services).length} service(s).`);
+		},
+
+		/**
+		 * Start a local service by name
+		 *
+		 * @param {String} serviceName
+		 */
+		startService(serviceName) {
+			const service = broker.getLocalService(serviceName);
+			if (service)
+				return;// throw new MoleculerClientError(`The '${serviceName}' service is already running`, 400, null, { service: serviceName });
+
+			const schema = Object.values(this.services).find(schema => schema.name == serviceName);
+			if (!schema)
+				throw new MoleculerClientError(`The '${serviceName}' service is not found`, 400, null, { service: serviceName });
+
+			broker.createService(schema);
+		},
+
+		/**
+		 * Stop a local running service by name
+		 *
+		 * @param {String} serviceName
+		 */
+		stopService(serviceName) {
+			const service = broker.getLocalService(serviceName);
+			if (!service)
+				throw new MoleculerClientError(`The '${serviceName}' service is not running`, 400, null, { service: serviceName });
+
+			broker.destroyService(service);
+		},
+
+		/**
+		 * Start all local services
+		 *
+		 */
+		startAllServices() {
+			Object.values(this.services).forEach(schema => {
+				try {
+					this.startService(schema.name);
+				} catch(ex) {
+					this.logger.warn(ex);
+				}
+			});
+		},
+
+		/**
+		 * Stop all local services
+		 *
+		 */
+		stopAllServices() {
+			broker.services
+				.filter(service => !/^\$/.test(service.name))
+				.forEach(service => this.stopService(service.name));
+		},
+
+		/**
+		 * Execute a command
+		 *
+		 * @param {String} cmd
+		 * @param {Object} opts
+		 */
+		execute(cmd, opts) {
+			this.logger.info(`Execute: ${cmd}`);
+			return new Promise((resolve, reject) => {
+				Exec(cmd, opts, (error, stdout, stderr) => {
+					if (error) {
+						this.logger.error("Error output:", error);
+						return reject(error);
+					}
+
+					this.logger.debug("Output:", stdout);
+					if (stderr)
+						this.logger.error("STDERR:", stderr);
+
+					resolve({ cmd, opts, stdout, stderr });
+				});
+			});
+		},
+
+		spawn(command, args, opts) {
+			this.logger.info("Spawn a new process.", command, args);
+			return Spawn(command, args, opts);
+		}
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+		this.services = [];
+
+		this.readServiceFolder();
+	}
+}
